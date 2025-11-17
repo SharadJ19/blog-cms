0bc318ff5d013e2b6e6f2b08801577eda0b0fda0
Sharad Chandel <sharadchandel2005@gmail.com>
2025-11-18 01:27:06 +0530

    feat(dashboard): add dashboard page with html/css/ts

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api-service';
+import { PostModel } from '../../models/post-model';
+import { RouterLink } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NgIf, NgFor, RouterLink],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard implements OnInit {
+  postsCount = 0;
+  categoriesCount = 0;
+  recentPosts: PostModel[] = [];
+
+  // we could also use inject keyword for using ApiService (modern way)
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    // fetching the last 5 posts
+    this.apiService.getPosts().subscribe(posts => {
+      this.postsCount = posts.length;
+      this.recentPosts = posts.slice(-5).reverse();
+    });
+
+    // fetching the no of categories
+    this.apiService.getCategories().subscribe(categories => {
+      this.categoriesCount = categories.length;
+    });
+  }
+}
