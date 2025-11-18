33f3e24031eeda2831a2d8a9ca0a969bcff15702
Sharad Chandel <sharadchandel2005@gmail.com>
2025-11-18 14:18:38 +0530

    feat(router): add dashboard, posts, post-form, post-detail, and categories routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Posts } from './pages/posts/posts';
+import { PostForm } from './pages/post-form/post-form';
+import { PostDetail } from './pages/post-detail/post-detail';
+import { Categories } from './pages/categories/categories';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'posts', component: Posts },
+  { path: 'posts/new', component: PostForm },
+  { path: 'posts/:id/edit', component: PostForm },
+  { path: 'posts/:id', component: PostDetail },
+  { path: 'categories', component: Categories },
+
+];
